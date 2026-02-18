7f2bd8e4b7734fa4d34b9a042388250b337ace21
sagar-317 <107807697+sagar-317@users.noreply.github.com>
2026-02-18 23:56:47 +0530

    Update MPFF-5PF.f

--- MPFF-5PF.f
+++ MPFF-5PF.f
@@ -1,7 +1,14 @@
 ! MPFF - framework - Sagar (latest edit - 24th June 2025)
-! Hybrid multi-phase-field fracture formulation based on 
-! Cuntze failure mode concept for progressive damage 
-! modelling of elasto-plastic FRP composites
+
+! Shiv Sagar, Shubhankar Roy Chowdhury,
+! Hybrid multi-phase-field fracture formulation based on Cuntze failure mode concept for progressive damage modelling of elasto-plastic FRP composites,
+! Composite Structures,
+! Volume 383,
+! 2026,
+! 120154,
+! ISSN 0263-8223,
+! https://doi.org/10.1016/j.compstruct.2026.120154.
+! (https://www.sciencedirect.com/science/article/pii/S0263822326001194)
 ! 
 ! Base Code:
 ! Wei Tan, Emilio Martinez-Paneda. 
@@ -2235,4 +2242,5 @@ C**********************************************************************
                         end do
 
                         return
+
       end subroutine dyadic
